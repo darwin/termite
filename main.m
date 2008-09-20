0dd461be98111851f48ad86ce57afcc541abba8f
Antonin Hildebrand <antonin@hildebrand.cz>
2008-09-20 19:54:52 +0200

    initial MacRuby application skeleton (version 0.3)

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  termite
+//
+//  Created by Antonin Hildebrand on 9/20/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("main.rb", argc, argv);
+}
